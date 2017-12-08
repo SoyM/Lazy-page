6f4e0241501b83e00f70bdf0719b7d2475cb6072
SoyM <huangxinjiang1996@gmail.com>
2017-12-08 14:56:16 +0800

    update admin

--- red/admin.py
+++ red/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-
 from .models import Paper
 
-admin.site.register(Paper)
+
+class PaperAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['content']
+    fieldsets = [
+        ('Main', {'fields': ['title', 'content']}),
+        ('Date', {'fields': ['pub_date']})
+    ]
+
+
+admin.site.register(Paper, PaperAdmin)
