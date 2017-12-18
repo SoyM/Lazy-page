b4b37d83b92b76966f4fcb5a1105e55f0c96038b
SoyM <huangxinjiang1996@gmail.com>
2017-12-18 23:40:32 +0800

    update urls

--- red/urls.py
+++ red/urls.py
@@ -4,11 +4,14 @@ from red import views
 app_name = 'red'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('login/', views.login, name='login'),
-    path('<int:pk>/', views.paper_detail, name='detail'),
-    path('panel/', views.panel, name='panel'),
-    path('post/', views.PostView.as_view(), name='post'),
     path('project/', views.ProjectView.as_view(), name='project'),
+    # auth
+    path('login/', views.login, name='login'),
+    # post
+    path('<int:pk>/', views.paper_detail, name='detail'),
+    path('<int:pk>/paper_edit/', views.paper_edit, name='paper_edit'),
+    path('post/', views.PostView.as_view(), name='post'),
+    # panel
+    path('panel/', views.panel, name='panel'),
     path('update_status/', views.update_status, name='update_status'),
-    path('<int:pk>/paper_edit/', views.paper_edit, name='paper_edit')
 ]
