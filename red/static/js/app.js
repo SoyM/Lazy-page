a41c68638ccaa3830fdabc8d45fa5c6095edc5da
SoyM <huangxinjiang1996@gmail.com>
2018-03-26 21:57:03 +0800

    样式更新

--- red/static/js/app.js
+++ red/static/js/app.js
@@ -1,45 +1,45 @@
-$(function() {
+$(function () {
 
-        var $fullText = $('.admin-fullText');
-        $('#admin-fullscreen').on('click', function() {
-            $.AMUI.fullscreen.toggle();
-        });
+    var $fullText = $('.admin-fullText');
+    $('#admin-fullscreen').on('click', function () {
+        $.AMUI.fullscreen.toggle();
+    });
 
-        $(document).on($.AMUI.fullscreen.raw.fullscreenchange, function() {
-            $fullText.text($.AMUI.fullscreen.isFullscreen ? '退出全屏' : '开启全屏');
-        });
+    $(document).on($.AMUI.fullscreen.raw.fullscreenchange, function () {
+        $fullText.text($.AMUI.fullscreen.isFullscreen ? '退出全屏' : '开启全屏');
+    });
 
-        var dataType = $('body').attr('data-type');
-        for (var key in pageData) {
-            if (key == dataType) {
-                pageData[key]();
-            }
+    var dataType = $('body').attr('data-type');
+    for (var key in pageData) {
+        if (key === dataType) {
+            pageData[key]();
         }
+    }
 
-        $('.tpl-switch').find('.tpl-switch-btn-view').on('click', function() {
-            $(this).prev('.tpl-switch-btn').prop("checked", function() {
-                    if ($(this).is(':checked')) {
-                        return false
-                    } else {
-                        return true
-                    }
-                })
+    $('.tpl-switch').find('.tpl-switch-btn-view').on('click', function () {
+        $(this).prev('.tpl-switch-btn').prop("checked", function () {
+            if ($(this).is(':checked')) {
+                return false
+            } else {
+                return true
+            }
         })
-    });
-    // ==========================
-    // 侧边导航下拉列表
-    // ==========================
+    })
+});
+// ==========================
+// 侧边导航下拉列表
+// ==========================
 
-$('.tpl-left-nav-link-list').on('click', function() {
-        $(this).siblings('.tpl-left-nav-sub-menu').slideToggle(80)
-            .end()
-            .find('.tpl-left-nav-more-ico').toggleClass('tpl-left-nav-more-ico-rotate');
-    });
-    // ==========================
-    // 头部导航隐藏菜单
-    // ==========================
+$('.tpl-left-nav-link-list').on('click', function () {
+    $(this).siblings('.tpl-left-nav-sub-menu').slideToggle(80)
+        .end()
+        .find('.tpl-left-nav-more-ico').toggleClass('tpl-left-nav-more-ico-rotate');
+});
+// ==========================
+// 头部导航隐藏菜单
+// ==========================
 
-$('.tpl-header-nav-hover-ico').on('click', function() {
+$('.tpl-header-nav-hover-ico').on('click', function () {
     $('.tpl-left-nav').toggle();
     $('.tpl-content-wrapper').toggleClass('tpl-content-wrapper-hover');
 });
